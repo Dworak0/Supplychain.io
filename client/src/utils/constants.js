ac02c6620d0f64de6f8b5d9b5605cfbd51e6ccf7
dworak0 <dworaksai48@gmail.com>
2026-02-27 20:04:52 +0530

    chore: update contract address to new Remix deployment with confirmReceipt

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from '../artifacts/contracts/SupplyChain.sol/SupplyChain.json';
 
-export const contractAddress = '0x46C369C06fBc693C9F293a46ad70C975ac01b4d7';
+export const contractAddress = '0x0957259114C1826C81034B5bFa0Bb613312855FC';
 export const contractABI = abi.abi;
